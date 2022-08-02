8e0c97e713f37fbff4a3470a337159867b0bccfc
Minchou99 <minhchaupt2903@gmail.com>
2022-08-02 17:05:29 +0700

    update

--- src/ss2_loop_in_java/exercise/array/AddElementToArray.java
+++ src/ss2_loop_in_java/exercise/array/AddElementToArray.java
@@ -1,4 +1,21 @@
 package ss2_loop_in_java.exercise.array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddElementToArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input Number: ");
+        int num = scanner.nextInt();
+        System.out.print("Input Index: ");
+        int index = scanner.nextInt();
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = num;
+        System.out.println(Arrays.toString(arr));
+    }
 }
+
