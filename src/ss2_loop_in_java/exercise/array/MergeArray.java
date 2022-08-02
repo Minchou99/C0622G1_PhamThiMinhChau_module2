282b58f2eb9b310d361469d8bd5be207d4b07b45
Minchou99 <minhchaupt2903@gmail.com>
2022-08-02 18:31:02 +0700

    update

--- src/ss2_loop_in_java/exercise/array/MergeArray.java
+++ src/ss2_loop_in_java/exercise/array/MergeArray.java
@@ -1,7 +1,40 @@
 package ss2_loop_in_java.exercise.array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MergeArray {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        do {
+            System.out.print("Nhập vào số phần tử của mảng 1: ");
+            num1 = scanner.nextInt();
+        } while (num1 <= 0);
+        int[] arr1 = new int[num1];
+        for (int i = 0; i < num1; i++) {
+            System.out.print("Nhập phần tử thứ " + i + ":");
+            arr1[i] = scanner.nextInt();
+        }
+        do {
+            System.out.print("Nhập vào số phần tử của mảng 2: ");
+            num2 = scanner.nextInt();
+        } while (num2 <= 0);
+        int[] arr2 = new int[num1];
+        for (int i = 0; i < num2; i++) {
+            System.out.print("Nhập phần tử thứ " + i + ":");
+            arr2[i] = scanner.nextInt();
+        }
+        int[] arr3 = new int[(num1 + num2)];
+        for (int i = 0; i < num1; i++) {
+            arr3[i] = arr1[i];
+        }
+        int j = 0;
+        for (int i = num1; i <= (num2 * 2); i++) {
+            arr3[i] = arr2[j];
+            j++;
+        }
+        System.out.print(Arrays.toString(arr3));
     }
 }
