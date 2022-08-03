d40011be06170acead721dccf1d496e694cc2555
Minchou99 <minhchaupt2903@gmail.com>
2022-08-03 15:03:35 +0700

    update

--- src/ss3_array_method/exercise/CountTheTimesOfNumber.java
+++ src/ss3_array_method/exercise/CountTheTimesOfNumber.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class CountTheTimesOfNumber {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        String str = "The Weather Is Bad";
+        System.out.print("Input Char: ");
+        char check = scanner.nextLine().charAt(0);
+        int count = 0;
+        int i;
+        for (i = 0; i < str.length(); i++) {
+            if (check == str.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
